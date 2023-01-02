1324b448c4a2dcc6dbb74cf24d4d184f384a46f3
Hunter Gerlach <hunter@huntergerlach.com>
2023-01-02 10:21:49 -0500

    Add floating tooth for contact button

---
+++ components/ContactTooth.tsx
@@ -0,0 +1,22 @@
+// A react component that displays a navbar at the top of the page
+import Image from 'next/image'
+
+export default function ContactTooth() {
+
+
+    return (
+      <div className="fixed bottom-0 right-0 mb-8 mr-8">
+        {/* A react component that displays a floating tooth icon at the bottom of the page that encourages the user to "Contact us!" */}
+        <a href="./contact-us">
+            {/* an image called tooth-svgrepo-com.svg */}
+            <Image
+            src="/floating_tooth.png"
+            alt="Tooth icon"
+            width={100}
+            height={100}
+            />
+        </a>
+    </div>
+    );
+}
+
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,9 +7,11 @@ export default function Header() {
     return (
       <>
         <MessageBanner id="new-patients" type="blue" message="New patients welcome!" link="/" linkText="Book your appointment today" />
-        <MessageBanner id="book" type="green" message="We are open!" link="/" linkText="Book your appointment today" />
-        <MessageBanner id="warn" type="red" message="We are closed!" link="/" linkText="Book your appointment today" />
-        <MessageBanner id="info" type="yellow" message="We are open!" link="/" linkText="Book your appointment today" />
+        {/*
+          <MessageBanner id="book" type="green" message="We are open!" link="/" linkText="Book your appointment today" />
+          <MessageBanner id="warn" type="red" message="We are closed!" link="/" linkText="Book your appointment today" />
+          <MessageBanner id="info" type="yellow" message="We are open!" link="/" linkText="Book your appointment today" />
+        */}
         
         < NavBar />
       </>
