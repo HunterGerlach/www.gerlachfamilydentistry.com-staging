c37bbb78cbedff85694116c9667bd3affc28de45
Hunter Gerlach <hunter@huntergerlach.com>
2023-01-05 13:34:20 -0500

    Add testimonials

--- components/Other.tsx
+++ components/Other.tsx
@@ -4,88 +4,6 @@ import Image from "next/image";
 export default function Other() {
   return (
     <div id="#other" className="bg-white text-black">
-      <div className="mx-auto flex flex-col items-center px-5 pt-56 lg:flex-row">
-        <div className="mb-16 flex flex-col text-left lg:mb-0 lg:w-1/2 lg:flex-grow lg:items-start lg:pr-16 lg:pr-6">
-          <h2 className="mb-4 text-4xl font-bold leading-none sm:text-5xl">
-            Bibendum tortor et sit convallis nec morbi.
-          </h2>
-          <p className="font-3xl mb-8 font-semibold leading-relaxed">
-            Lorem ipsum auctor sit amet, consectetur adipiscing elit. Sit a
-            egestas tortor viverra nisl, in non. Neque viverra sollicitudin amet
-            volutpat auctor amet. Aliquam pellentesque condimentum mauris sit
-            tincidunt egestas ullamcorper sit.{" "}
-          </p>
-        </div>
-        <div className="lg:w-full lg:max-w-2xl">
-          <Image
-            src="https://picsum.photos/648/480?"
-            alt="img"
-            width={648}
-            height={480}
-          />
-        </div>
-      </div>
-      <div className="mt-32">
-        <div className="mx-auto flex flex-col px-5 py-24 text-left lg:flex-row">
-          <div className="hidden lg:inline-block lg:w-full lg:max-w-xl">
-            <Image
-              src="https://picsum.photos/648/480?"
-              alt="img"
-              width={648}
-              height={480}
-            />
-          </div>
-          <div className="flex flex-col pt-0 text-left lg:w-1/2 lg:flex-grow lg:items-start lg:pl-16 lg:pl-24 lg:pt-24">
-            <h2 className="mb-4 text-4xl font-bold leading-none sm:text-5xl">
-              Eu diam in magna blandit sit magna dolor proin velit.
-            </h2>
-            <p className="mb-8 font-semibold leading-relaxed text-black">
-              Lorem ipsum ac neque, consectetur adipiscing elit. Nibh neque, ut
-              purus donec sed donec semper ac vestibulum. Mi urna, facilisis
-              arcu, auctor elit. Ut nunc non aenean netus ut.
-            </p>
-          </div>
-          <div className="inline-block lg:hidden lg:w-full lg:max-w-xl">
-            <Image
-              src="https://picsum.photos/648/480?"
-              alt="img"
-              width={648}
-              height={480}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="my-24 p-4 text-black">
-        <div className="max-w-9xl mx-auto flex flex-col items-center bg-gradient-to-r from-blue-200 to-blue-100 px-5 py-24 lg:flex-row">
-          <div className="flex flex-col items-center pb-16 pl-0 text-center lg:mb-0 lg:w-1/2 lg:flex-grow lg:items-start lg:pl-12 lg:pr-24 lg:text-left">
-            <h2 className="pb-4 text-2xl font-bold leading-tight lg:text-4xl">
-              Lorem ipsum mi at amet, consecteturadipiscing elit. Mattis.
-            </h2>
-            <p className="text-md mb-8 lg:text-xl">
-              Lorem ipsum praesent amet, consectetur adipiscing elit. Cursus
-              ullamcorper id tristique tincidunt. Tincidunt feugiat at mi
-              feugiat hendrerit. Ac faucibus accumsan, quis lacus, lectus eget
-              bibendum. At praesent quisque sollicitudin fusce.
-            </p>
-          </div>
-          <div className="w-4/7 pr-12 lg:w-2/5">
-            <Image
-              src="https://picsum.photos/648/480?"
-              className="hidden object-cover object-center lg:inline-block"
-              alt="image"
-              width={648}
-              height={480}
-            />
-            <Image
-              src="https://picsum.photos/648/480?"
-              className="inline-block object-cover object-center lg:hidden"
-              alt="image"
-              width={648}
-              height={480}
-            />
-          </div>
-        </div>
-      </div>
       <div className="mx-auto">
         <div className="max-w-8xl mx-auto px-5 py-24 lg:px-24">
           <div className="my-6 flex w-full flex-col text-left lg:text-center">
@@ -134,6 +52,64 @@ export default function Other() {
             </Link>
           </div>
         </div>
+      </div>
+      <div
+        className="
+              max-w-9xl
+              mx-auto
+              flex
+              flex-col
+              items-center
+              justify-center
+              px-5
+            "
+      >
+        <div className="mr-0 mb-6 w-full py-4 text-center lg:w-2/3">
+          <h2 className="mb-4 text-4xl font-bold sm:text-5xl">
+            Sem enim cursus orci at.
+          </h2>
+          <p className="mb-4 text-lg leading-relaxed">
+            In ullamcorper magna nunc, non molestie augue feugiat eget. Mauris,
+            vitae et, vitae et cursus amet tincidunt feugiat nulla. Senectus
+            maecenas diam risus sodales dictum eu. Eget cursus sit bibendum
+            pulvinar faucibus vitae nam sed. Faucibus vel laoreet.
+          </p>
+          <Link href="/" className="underline-blue font-semibold">
+            Learn more
+          </Link>
+        </div>
+        <Image
+          className="
+                lg:w-5/7
+                mb-40
+                hidden
+                w-5/6
+                rounded object-cover
+                object-center
+                lg:inline-block 
+                lg:w-4/6
+              "
+          src="https://picsum.photos/648/480?"
+          alt="img"
+          width={648}
+          height={480}
+        />
+
+        <Image
+          className="
+              mb-24
+              inline-block
+              w-5/6
+              rounded
+              object-cover object-center
+              lg:hidden
+              lg:w-4/6 
+            "
+          src="https://picsum.photos/648/480?"
+          alt="img"
+          width={648}
+          height={480}
+        />
       </div>
     </div>
   );
--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -3,64 +3,124 @@ import Link from "next/link";
 
 export default function Reviews() {
   return (
-    <div id="reviews" className="text-black">
-      <div
-        className="
-              max-w-9xl
-              mx-auto
-              flex
-              flex-col
-              items-center
-              justify-center
-              px-5
-            "
-      >
-        <div className="mr-0 mb-6 w-full py-4 text-center lg:w-2/3">
-          <h2 className="mb-4 text-4xl font-bold sm:text-5xl">
-            Sem enim cursus orci at.
-          </h2>
-          <p className="mb-4 text-lg leading-relaxed">
-            In ullamcorper magna nunc, non molestie augue feugiat eget. Mauris,
-            vitae et, vitae et cursus amet tincidunt feugiat nulla. Senectus
-            maecenas diam risus sodales dictum eu. Eget cursus sit bibendum
-            pulvinar faucibus vitae nam sed. Faucibus vel laoreet.
-          </p>
-          <Link href="/" className="underline-blue font-semibold">
-            Learn more
-          </Link>
+    <div
+      id="reviews"
+      className="text-black max-w-8xl mx-auto px-5 py-24 lg:px-24"
+    >
+      <div className="my-6 flex w-full flex-col text-left lg:text-center">
+        <h3 className="mb-8 text-5xl font-bold text-black">
+          Patient Testimonials: A Reason to Smile
+        </h3>
+        <h3 className="mb-12 px-0 text-lg font-semibold text-gray-900 lg:px-52">
+          Read reviews from real patients to see why our family dentistry office
+          is the go-to for top-notch treatment and personalized service. Patient
+          testimonials offer a firsthand look at the care and attention our team
+          provides. Find out what our satisfied patients have to say and why
+          they keep coming back for more.
+        </h3>
+      </div>
+      <div className="my-24 p-4 text-black">
+        <div className="max-w-9xl mx-auto flex flex-col items-center bg-gradient-to-r from-blue-200 to-blue-100 px-5 py-24 lg:flex-row">
+          <div className="flex flex-col items-center pb-16 pl-0 text-center lg:mb-0 lg:w-1/2 lg:flex-grow lg:items-start lg:pl-12 lg:pr-24 lg:text-left">
+            <h2 className="pb-4 text-2xl font-bold leading-tight lg:text-4xl">
+              "I am always blown away by the level of care and attention I
+              receive."
+            </h2>
+            <p className="text-md mb-8 lg:text-xl">
+              I have been going to Dr. Gerlach's office for years, and I am
+              always blown away by the level of care and attention I receive.
+              From the moment I walk in the door, I feel welcomed and valued.
+              The hygienists are gentle and thorough, and Dr. Smith is always
+              willing to take the time to answer my questions and address my
+              concerns. I can't recommend this practice enough!
+            </p>
+          </div>
+          <div className="w-4/7 pr-12 lg:w-2/5">
+            <Image
+              src="https://picsum.photos/648/480?"
+              className="hidden object-cover object-center lg:inline-block"
+              alt="image"
+              width={648}
+              height={480}
+            />
+            <Image
+              src="https://picsum.photos/648/480?"
+              className="inline-block object-cover object-center lg:hidden"
+              alt="image"
+              width={648}
+              height={480}
+            />
+          </div>
         </div>
-        <Image
-          className="
-                lg:w-5/7
-                mb-40
-                hidden
-                w-5/6
-                rounded object-cover
-                object-center
-                lg:inline-block 
-                lg:w-4/6
-              "
-          src="https://picsum.photos/648/480?"
-          alt="img"
-          width={648}
-          height={480}
-        />
-
-        <Image
-          className="
-              mb-24
-              inline-block
-              w-5/6
-              rounded
-              object-cover object-center
-              lg:hidden
-              lg:w-4/6 
-            "
-          src="https://picsum.photos/648/480?"
-          alt="img"
-          width={648}
-          height={480}
-        />
+      </div>
+      <div className="mt-32">
+        <div className="mx-auto flex flex-col px-5 py-24 text-left lg:flex-row">
+          <div className="hidden lg:inline-block lg:w-full lg:max-w-xl">
+            <Image
+              src="https://picsum.photos/648/480?"
+              alt="img"
+              width={648}
+              height={480}
+              className="block object-cover object-center w-full h-full"
+            />
+          </div>
+          <div className="flex flex-col pt-0 text-left lg:w-1/2 lg:flex-grow lg:items-start lg:pl-16 lg:pl-24 lg:pt-24">
+            <h2 className="mb-4 text-4xl font-bold leading-none sm:text-5xl">
+              "They explained everything in a way she could understand and made
+              her feel comfortable and relaxed."
+            </h2>
+            <p className="mb-8 font-semibold leading-relaxed text-black">
+              My daughter was really nervous about getting her first filling,
+              but the team at Dr. Smith's office was amazing. They explained
+              everything in a way she could understand and made her feel
+              comfortable and relaxed. She even fell asleep during the
+              procedure! I am so grateful to have found such a kind and
+              compassionate team to take care of our dental needs.
+            </p>
+          </div>
+          <div className="inline-block lg:hidden lg:w-full lg:max-w-xl">
+            <Image
+              src="https://picsum.photos/648/480?"
+              alt="img"
+              width={648}
+              height={480}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="my-24 p-4 text-black">
+        <div className="max-w-9xl mx-auto flex flex-col items-center bg-gradient-to-r from-blue-200 to-blue-100 px-5 py-24 lg:flex-row">
+          <div className="flex flex-col items-center pb-16 pl-0 text-center lg:mb-0 lg:w-1/2 lg:flex-grow lg:items-start lg:pl-12 lg:pr-24 lg:text-left">
+            <h2 className="pb-4 text-2xl font-bold leading-tight lg:text-4xl">
+              "I barely felt a thing! They even gave me tips for how to take
+              better care of my teeth at home."
+            </h2>
+            <p className="text-md mb-8 lg:text-xl">
+              I had been putting off getting my teeth cleaned for a while
+              because I was worried about the pain and discomfort. But the team
+              at Dr. Smith's office was so gentle and gentle, I barely felt a
+              thing! They even gave me tips for how to take better care of my
+              teeth at home. I left feeling confident and motivated to take
+              better care of my oral health. Thank you, Dr. Smith and team!
+            </p>
+          </div>
+          <div className="w-4/7 pr-12 lg:w-2/5">
+            <Image
+              src="https://picsum.photos/648/480?"
+              className="hidden object-cover object-center lg:inline-block"
+              alt="image"
+              width={648}
+              height={480}
+            />
+            <Image
+              src="https://picsum.photos/648/480?"
+              className="inline-block object-cover object-center lg:hidden"
+              alt="image"
+              width={648}
+              height={480}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
